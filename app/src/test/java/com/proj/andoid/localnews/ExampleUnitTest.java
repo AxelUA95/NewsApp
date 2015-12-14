0c14173d5f851caf61da6243b373d5699eeae93c
NastaNe <NastaNe@gmail.com>
2015-12-14 22:01:36 +0200

    dummy test

--- app/src/test/java/com/proj/andoid/localnews/ExampleUnitTest.java
+++ app/src/test/java/com/proj/andoid/localnews/ExampleUnitTest.java
@@ -14,6 +14,7 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+        assertNotEquals(2*2, 10);
     }
 
     @Test
